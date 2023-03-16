9aff0addf31784e54b50f6fb2b3ebc5d997efe0f
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-03-16 20:46:36 -0300

    feat: css home and create ClaimUsernameForm component

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+
+import { Heading, Text } from '@ignite-ui/react'
+
+import previewImage from '../../assests/app-preview.png'
+
+import { Container, Hero, Preview } from './styles'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl" as="h1">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="lg">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+
+        <ClaimUsernameForm />
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  )
+}
